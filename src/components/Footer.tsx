593bc424af7fe6589c2486b37afa009cf09cd5f3
Patrice Douge <patricedouge@gmail.com>
2024-06-23 02:21:48 -0400

    footer updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,19 +4,72 @@ import { Container } from "@/components/Container";
 import { Logo } from "@/components/Logo";
 import { NavLink } from "@/components/NavLink";
 
+import {
+  BuildingOffice2Icon,
+  EnvelopeIcon,
+  PhoneIcon,
+} from "@heroicons/react/24/outline";
+
 export function Footer() {
   return (
     <footer>
       <Container>
-        <div className="py-16">
+        <div className="py-16 flex flex-col items-center space-y-12 text-center">
           <Logo className="mx-auto h-10 w-auto" />
           <nav className="mt-10 text-sm" aria-label="quick links">
-            <div className="-my-1 flex justify-center gap-x-6">
+            <div className="-my-1 gap-x-6">
               <NavLink href="#features">Client Solutions</NavLink>
               <NavLink href="#testimonials">Process</NavLink>
               <NavLink href="#pricing">Careers</NavLink>
             </div>
           </nav>
+          <dl className="space-y-4 text-base text-gray-600">
+            <div className="flex gap-x-4">
+              <dt className="flex-none">
+                <span className="sr-only">Address</span>
+                <BuildingOffice2Icon
+                  className="h-7 w-6 text-gray-400"
+                  aria-hidden="true"
+                />
+              </dt>
+              <dd>
+                115 E. Main St.
+                <br />
+                Buford, GA 30518
+              </dd>
+            </div>
+            <div className="flex gap-x-4">
+              <dt className="flex-none">
+                <span className="sr-only">Telephone</span>
+                <PhoneIcon
+                  className="h-7 w-6 text-gray-400"
+                  aria-hidden="true"
+                />
+              </dt>
+              <dd>
+                <a className="hover:text-gray-900" href="tel:+18007771123">
+                  +1(800) 777-1123
+                </a>
+              </dd>
+            </div>
+            <div className="flex gap-x-4">
+              <dt className="flex-none">
+                <span className="sr-only">Email</span>
+                <EnvelopeIcon
+                  className="h-7 w-6 text-gray-400"
+                  aria-hidden="true"
+                />
+              </dt>
+              <dd>
+                <a
+                  className="hover:text-gray-900"
+                  href="mailto:hello@example.com"
+                >
+                  contact@creolepro.com
+                </a>
+              </dd>
+            </div>
+          </dl>
         </div>
         <div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
           <p className="mt-6 text-sm text-slate-500 sm:mt-0">
