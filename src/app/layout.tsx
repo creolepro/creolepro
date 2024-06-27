95231eaf9f0e21192ca8b882d8cb07dc033335f6
Patrice Douge <patricedouge@gmail.com>
2024-06-26 22:55:01 -0400

    capitalize creolepro desc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Creolepro | Haitian Creole Language Services – Human Translation",
   description:
-    "creolepro offers professional Haitian Creole translation services for businesses, organizations, and individuals. Our human translators provide accurate and culturally appropriate translations to help you communicate effectively with your Haitian Creole-speaking community.",
+    "Creolepro offers professional Haitian Creole translation services for businesses, organizations, and individuals. Our human translators provide accurate and culturally appropriate translations to help you communicate effectively with your Haitian Creole-speaking community.",
 };
 
 export default function RootLayout({
